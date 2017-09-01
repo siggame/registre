f6cd2d6a2e24ca9e83e10911a1f804cc964ca94e
Reno DuBois <rdubois96@gmail.com>
2017-09-01 16:13:25 -0500

    Converted to EJS, validation working

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import * as express from "express";
 import * as csv from "./csv";
 import * as vars from "./vars";
 import * as bodyParser from "body-parser";
+import { body, validationResult} from "express-validator/check";
 
 winston.configure({
     transports: [
@@ -20,18 +21,32 @@ winston.configure({
 const app = express();
 
 app.use(express.static('static'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 // Body parser for handling request data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/user_input', (req: Request, res: Response) => {
-    csv.writeRow(req.body).then(() => {
-        // TODO: Validation and redirection to actual pages
-        res.redirect('/');
-    }).catch((err) => {
-        winston.error(err);
-        res.redirect('/500/');
-    });
+app.get('/', (req: Request, res: Response) => {
+   res.render('form', {errors: []});
+});
+
+app.post('/', [
+    body('name', 'Please enter a name').not().isEmpty(),
+    body('email', 'Please enter an email').not().isEmpty(),
+    body('email', 'Invalid email - please use a valid email address.').isEmail(),
+    ], (req: Request, res: Response) => {
+    const errors = validationResult(req).array();
+    if(errors.length > 0) {
+        res.render('form', {errors: errors});
+    } else {
+        csv.writeRow(req.body).then(() => {
+            res.redirect('/submitted.html');
+        }).catch((err) => {
+            winston.error(err);
+            res.redirect('/error.html');
+        });
+    }
 });
 
 app.listen(vars.PORT, () => {
